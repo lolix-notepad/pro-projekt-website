56a2b6d23666ba93eb1fa634555c035f7f2b4101
example <example@test.com>
2021-07-31 15:21:40 -0500

    fixed error with image loader 6.0

--- components/image-loader.tsx
+++ components/image-loader.tsx
@@ -3,7 +3,7 @@ interface IImageLoader {
 }
 
 const imageLoader = ({ src }: IImageLoader) => {
-    return `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcolorlib.com%2Fwp%2Fwp-content%2Fuploads%2Fsites%2F2%2F404-error-template-16.png&f=1&nofb=1`
+    return `./${src}`
 }
 
 export default imageLoader
