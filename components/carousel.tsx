ab876bbd5d7b067c55eba032f4b7e53635425518
example <example@test.com>
2021-07-31 13:07:41 -0500

    fixed error type, and added temporal image error

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
+emport Image from 'next/image'
 
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade])
 
-const imageLoader = ({ src }) => {
-    return `https://github.com/`
+interface IImageLoader {
+  src: string
+}
+
+const imageLoader = ({ src }: IImageLoader) => {
+    return `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcolorlib.com%2Fwp%2Fwp-content%2Fuploads%2Fsites%2F2%2F404-error-template-16.png&f=1&nofb=1`
 }
 
 export default function Carousel() {
